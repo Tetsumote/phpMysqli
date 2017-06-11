4cbbaa60860dcaf7b641eea409560692ce8ac961
tetsumote <unionproject88@gmail.com>
2017-06-11 14:43:19 +0200

    Connection to database

---
+++ PhpConnection.php
@@ -0,0 +1,28 @@
+<?php
+//1. Create a database connection
+$dbhost = "localhost";
+$dbuser = "lynda";
+$dbpass = "lynda";
+$dbname = "lynda";
+$connection = mysqli_connect($dbhost,$dbuser,$dbpass,$dbname);
+//Test if connection occurred.
+if()
+
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Php connection</title>
+</head>
+<body>
+    
+
+
+
+
+</body>
+</html>
