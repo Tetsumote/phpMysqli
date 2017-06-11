bfc2a234542b5aaf76d1de5c038671b797ecc2c9
Tetsumote <unionproject88@gmail.com>
2017-06-11 15:30:24 +0200

    com03 var_dump on data

--- PhpConnection.php
+++ PhpConnection.php
@@ -1,16 +1,29 @@
 <?php
-//1. Create a database connection
+// 1. Create a database connection
 $dbhost = "localhost";
 $dbuser = "lynda";
 $dbpass = "lynda";
 $dbname = "lynda";
 $connection = mysqli_connect($dbhost,$dbuser,$dbpass,$dbname);
-//Test if connection occurred.
+// Test if connection occurred.
 if(mysqli_connect_errno()){
     die("Database connection failed: " .
     mysqli_connect_error() .
     " (" . mysqli_connect_errno() . ")"
     );
+}
+?>
+<?php
+// 2.Perform database query
+// $query = "SELECT * FROM subjects";
+$query = "SELECT * ";
+$query .= "FROM subjects ";
+$query .= "WHERE visible = 1 ";
+$query .= "ORDER BY position ASC";
+$result = mysqli_query($connection,$query);
+// Test if there was a query error
+if(!$result){
+    die("Database query failed.");
 }
 ?>
 <!DOCTYPE html>
@@ -24,8 +37,23 @@ if(mysqli_connect_errno()){
 <body>
     
 
+<?php
+// 3. Use returned data (if any)
+    while($row = mysqli_fetch_row($result)){
+        //output data from each row
+        var_dump($row);
+        echo "<hr />";
+    }
 
-
+?>
+<?php
+// 4.Release returned data
+mysqli_free_result($result);
+?>
 
 </body>
 </html>
+<?php
+// 5.Close database connection
+mysqli_close($connection);
+?>
